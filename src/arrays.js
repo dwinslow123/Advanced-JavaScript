86dc1e42a1e96d8aec7b4ba9ea83a49a66287eaa
Dan Winslow <dwinslow123@me.com>
2017-09-14 17:42:51 -0700

    some progress

--- src/arrays.js
+++ src/arrays.js
@@ -6,17 +6,17 @@ const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
   // based off http://underscorejs.org/#each
-  for (let i = 0; i < elements.length; i++) {
-    cb(elements[i], i);
-  }
+  elements.forEach((element, i) => {
+    cb(element, i);
+  });
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
-  const newArray = [];
-  each(elements, item => (newArray.push(cb(item))));
-  return newArray;
+  return elements.map((element) => {
+    return cb(element);
+  });
 };
 
 const reduce = (elements, cb, memo = elements.shift()) => {
@@ -33,16 +33,26 @@ const find = (elements, cb) => {
   // Look through each value in `elements` and pass each element to `cb`.
   // If `cb` returns `true` then return that element.
   // Return `undefined` if no elements pass the truth test.
+  for (let i = 0; i < elements.length; i++) {
+    if (cb(elements[i])) return elements[i];
+  }
+  return undefined;
 };
 
 const filter = (elements, cb) => {
   // Similar to `find` but you will return an array of all elements that passed the truth test
   // Return an empty array if no elements pass the truth test
+  return elements.filter((element) => {
+    if (elements[element]) {
+      return elements[element];
+    } return [];
+  });
 };
 
 const flatten = (elements) => {
-  // Flattens a nested array (the nesting can be to any depth).
-  // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  return elements.reduce((flat, toFlatten) => {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
 };
 
 /* eslint-enable no-unused-vars, max-len */
